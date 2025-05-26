e29742abfbbe4d28e551db66523ac0afb946a7bc
Lahiru Prasad <104630433+Lahiru-LK@users.noreply.github.com>
2025-05-26 15:41:05 +0530

    main up

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import MainBG from './components/mainbg';
+
+function App() {
+  return (
+    <div>
+      <h1>Vite + Flask Fullstack App</h1>
+      <MainBG />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/mainbg.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+function MainBG() {
+  const [message, setMessage] = useState('');
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+  useEffect(() => {
+    fetch(`${API_BASE_URL}/`)
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(err => console.error("Error fetching from backend:", err));
+  }, []);
+
+  return (
+    <div className="card">
+      <p>🟢 Backend Message: <strong>{message}</strong></p>
+    </div>
+  );
+}
+
+export default MainBG;
