bc9a65dc2f27de778bdeb61404c06b37a2efdbb8
Lahiru Prasad <104630433+Lahiru-LK@users.noreply.github.com>
2025-05-26 20:10:18 +0530

    Update mainbg.jsx

--- client/src/components/mainbg.jsx
+++ client/src/components/mainbg.jsx
@@ -5,6 +5,8 @@ function MainBG() {
   const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
   console.log("✅ API_BASE_URL:", API_BASE_URL);
+  console.log("🌐 Full ENV:", import.meta.env);
+
 
   
   useEffect(() => {
